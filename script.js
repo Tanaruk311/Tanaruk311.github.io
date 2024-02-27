169b6950cb3a1ae18a19b477d6c52594c1f21f1e
Tanaruk311 <156327029+Tanaruk311@users.noreply.github.com>
2024-02-27 13:51:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+ var product = [{
+    id: 1,
+    img:'https://images.unsplash.com/photo-1542629458-eaa56d608062?q=80&w=1467&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Glasses',
+    price: 2500,
+},{
+    id: 2,
+    img:'https://images.unsplash.com/photo-1434389677669-e08b4cac3105?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Women shirt',
+    price: 1200,
+},{
+    id: 3,
+    img:'https://images.unsplash.com/photo-1499971856191-1a420a42b498?q=80&w=1362&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Sweater',
+    price: 1500,
+},{
+    id: 4,
+    img:'https://images.unsplash.com/photo-1617331721458-bd3bd3f9c7f8?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Jacket',
+    price: 3200,
+},{
+    id: 5,
+    img:'https://images.unsplash.com/photo-1611911813383-67769b37a149?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Knit shirt',
+    price: 2800,
+
+},{
+    id: 6,
+    img:'https://images.unsplash.com/photo-1609172795052-05bf80681031?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Prada',
+    price: 64000,
+
+
+},{
+    id: 7,
+    img:'https://images.unsplash.com/photo-1574201635302-388dd92a4c3f?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'White sweater',
+    price: 3200,
+
+},{
+    id: 8,
+    img:'https://plus.unsplash.com/premium_photo-1675183713626-81e6de4552e1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Brown coat',
+    price: 5500,
+
+},{
+    id: 9,
+    img:'https://images.unsplash.com/photo-1521223890158-f9f7c3d5d504?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8amFja2V0fGVufDB8fDB8fHww',
+    name: 'Black jacket',
+    price: 3200,
+},{
+    id: 10,
+    img:'https://images.unsplash.com/photo-1560769629-975ec94e6a86?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Shoe',
+    price: 3800,
+},{
+    id: 11,
+    img:'"https://images.unsplash.com/photo-1596993100471-c3905dafa78e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Fashion Jacket',
+    price: 5500,
+},{
+    id: 12,
+    img:'https://images.unsplash.com/photo-1669479032946-b995b9c43917?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D',
+    name: ' Street Jacket',
+    price: 2800,
+},{
+    id: 13,
+    img:'https://images.unsplash.com/photo-1611911813383-67769b37a149?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Knit shirt',
+    price: 2800,
+},{
+    id: 14,
+    img:'https://images.unsplash.com/photo-1616088728028-a37e34f504c7?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Blue Jacket',
+    price: 4500,
+},{
+    id: 15,
+    img:'https://images.unsplash.com/photo-1644483878392-206392bffd5c?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Cardigan',
+    price: 2600,
+},{
+    id: 16,
+    img:'https://images.unsplash.com/photo-1638780853522-1ef7f826cd1a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Blue shirt',
+    price: 1700,
+
+}];
+
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("myBtn");
+
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
